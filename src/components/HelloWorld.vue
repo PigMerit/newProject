3503f5017964c55d84e9e2df36b0794c926ac1db
merit <whu@liuzhenpeng.com>
2020-10-13 10:19:03 +0800

    创建项目

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="hello">
+    {{aaa.value}}
+    <div @click='btnclick'>按钮</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data(){
+    return {
+      aaa:{
+        value:"yanzhang"
+      }
+    }
+  },
+  props: {
+    msg: String,
+  },
+  methods:{
+    btnclick(){
+      this.aaa.value = 'bbb';
+    }
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
